3f1258cb61507f688620bee897b2a24cd84276ca
Yi-Heng Liu <sdavidliu@users.noreply.github.com>
2017-07-13 14:21:28 -0700

    Add new zones and altitude

--- LongLat/ViewController.swift
+++ LongLat/ViewController.swift
@@ -47,7 +47,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         let o6 = CLLocationCoordinate2D(latitude: 33.649284, longitude: -117.842965)
         let o7 = CLLocationCoordinate2D(latitude: 33.649106, longitude: -117.842592)
         let o8 = CLLocationCoordinate2D(latitude: 33.649108, longitude: -117.842529)
-        let o9 = CLLocationCoordinate2D(latitude: 33.649128, longitude: -117.842422)
+        let o9 = CLLocationCoordinate2D(latitude: 33.649124, longitude: -117.842391)
         let o10 = CLLocationCoordinate2D(latitude: 33.648994, longitude: -117.842378)
         let o11 = CLLocationCoordinate2D(latitude: 33.648974, longitude: -117.842540)
         let o12 = CLLocationCoordinate2D(latitude: 33.648930, longitude: -117.842913)
@@ -63,35 +63,37 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         let o22 = CLLocationCoordinate2D(latitude: 33.648922, longitude: -117.842213)
         let o23 = CLLocationCoordinate2D(latitude: 33.648429, longitude: -117.842470)
         let o24 = CLLocationCoordinate2D(latitude: 33.648412, longitude: -117.842272)
-        let o25 = CLLocationCoordinate2D(latitude: 33.648357, longitude: -117.842564)
+        let o25 = CLLocationCoordinate2D(latitude: 33.648415, longitude: -117.842521)
         let o26 = CLLocationCoordinate2D(latitude: 33.648401, longitude: -117.842653)
         let o27 = CLLocationCoordinate2D(latitude: 33.648384, longitude: -117.842808)
         let o28 = CLLocationCoordinate2D(latitude: 33.648201, longitude: -117.842784)
-        let o29 = CLLocationCoordinate2D(latitude: 33.648133, longitude: -117.842552)
+        let o29 = CLLocationCoordinate2D(latitude: 33.648137, longitude: -117.842507)
         let o30 = CLLocationCoordinate2D(latitude: 33.648115, longitude: -117.842255)
         let o31 = CLLocationCoordinate2D(latitude: 33.648058, longitude: -117.841918)
-        let o32 = CLLocationCoordinate2D(latitude: 33.648406, longitude: -117.842160)
-        let o33 = CLLocationCoordinate2D(latitude: 33.648288, longitude: -117.841919)
+        let o32 = CLLocationCoordinate2D(latitude: 33.648444, longitude: -117.842138)
+        let o33 = CLLocationCoordinate2D(latitude: 33.648345, longitude: -117.841930)
         let o34 = CLLocationCoordinate2D(latitude: 33.648454, longitude: -117.841553)
-        let o35 = CLLocationCoordinate2D(latitude: 33.648538, longitude: -117.841707)
+        let o35 = CLLocationCoordinate2D(latitude: 33.648557, longitude: -117.841747)
         let o36 = CLLocationCoordinate2D(latitude: 33.648779, longitude: -117.841246)
-        let o37 = CLLocationCoordinate2D(latitude: 33.648876, longitude: -117.841410)
+        let o37 = CLLocationCoordinate2D(latitude: 33.648891, longitude: -117.841427)
         let o38 = CLLocationCoordinate2D(latitude: 33.648977, longitude: -117.841093)
         let o39 = CLLocationCoordinate2D(latitude: 33.649074, longitude: -117.841263)
         let o40 = CLLocationCoordinate2D(latitude: 33.649271, longitude: -117.841557)
         let o41 = CLLocationCoordinate2D(latitude: 33.649148, longitude: -117.841684)
-        let o42 = CLLocationCoordinate2D(latitude: 33.648932, longitude: -117.841856)
-        let o43 = CLLocationCoordinate2D(latitude: 33.649123, longitude: -117.842191)
+        let o42 = CLLocationCoordinate2D(latitude: 33.649415, longitude: -117.842193)
+        let o43 = CLLocationCoordinate2D(latitude: 33.649654, longitude: -117.842641)
         let o44 = CLLocationCoordinate2D(latitude: 33.649388, longitude: -117.841934)
         let o45 = CLLocationCoordinate2D(latitude: 33.649468, longitude: -117.842148)
         let o46 = CLLocationCoordinate2D(latitude: 33.649634, longitude: -117.841976)
-        let o47 = CLLocationCoordinate2D(latitude: 33.649562, longitude: -117.842417)
+        let o47 = CLLocationCoordinate2D(latitude: 33.649621, longitude: -117.842349)
         let o48 = CLLocationCoordinate2D(latitude: 33.649843, longitude: -117.842233)
         let o49 = CLLocationCoordinate2D(latitude: 33.649702, longitude: -117.842598)
         let o50 = CLLocationCoordinate2D(latitude: 33.650087, longitude: -117.842677)
         let o51 = CLLocationCoordinate2D(latitude: 33.649862, longitude: -117.842911)
         let o52 = CLLocationCoordinate2D(latitude: 33.649667, longitude: -117.843039)
         let o53 = CLLocationCoordinate2D(latitude: 33.649549, longitude: -117.842829)
+        let o54 = CLLocationCoordinate2D(latitude: 33.649267, longitude: -117.842505)
+        let o55 = CLLocationCoordinate2D(latitude: 33.649201, longitude: -117.842400)
         
         let oz1Points = OutsideZone(name: "OZ1", points: [o1,o2,o3,o4,o5,o6])
         let oz2Points = OutsideZone(name: "OZ2", points: [o5,o4,o7,o8,o11,o12,o13,o14])
@@ -108,14 +110,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         let oz13Points = OutsideZone(name: "OZ13", points: [o36,o37,o39,o38])
         let oz14Points = OutsideZone(name: "OZ14", points: [o37,o39,o40,o41])
         let oz15Points = OutsideZone(name: "OZ15", points: [o40,o41,o44,o46])
-        let oz16Points = OutsideZone(name: "OZ16", points: [o41,o42,o43,o44])
+        let oz16Points = OutsideZone(name: "OZ16", points: [o42,o43,o49,o47,o45])
         let oz17Points = OutsideZone(name: "OZ17", points: [o44,o45,o47,o48,o46])
         let oz18Points = OutsideZone(name: "OZ18", points: [o48,o47,o49,o50])
         let oz19Points = OutsideZone(name: "OZ19", points: [o49,o50,o51,o52,o53])
         let oz20Points = OutsideZone(name: "OZ20", points: [o52,o53,o1,o16])
         let oz21Points = OutsideZone(name: "OZ21", points: [o16,o15,o13,o14,o5,o6,o1])
+        let oz22Points = OutsideZone(name: "OZ22", points: [o4,o7,o8,o9,o55,o54])
         
-        outsideZones = [oz1Points,oz2Points,oz3Points,oz4Points,oz5Points,oz6Points,oz7Points,oz8Points,oz9Points,oz10Points,oz11Points,oz12Points,oz13Points,oz14Points,oz15Points,oz16Points,oz17Points,oz18Points,oz19Points,oz20Points,oz21Points]
+        outsideZones = [oz1Points,oz2Points,oz3Points,oz4Points,oz5Points,oz6Points,oz7Points,oz8Points,oz9Points,oz10Points,oz11Points,oz12Points,oz13Points,oz14Points,oz15Points,oz16Points,oz17Points,oz18Points,oz19Points,oz20Points,oz21Points,oz22Points]
         
         for oz in outsideZones {
             let polygon = MKPolygon(coordinates: &oz.points, count: oz.points.count)
@@ -148,7 +151,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             }
         }
         
-        label2.text = "Current location: \(zoneString)"
+        let alt = round(currentLocation.altitude*100)/100
+        var level = "Lower"
+        if (alt > 13.0){
+            level = "Upper"
+        }
+        
+        
+        label2.text = "Current location:\(zoneString)\nAlt:\(alt) Acc:\(round(currentLocation.verticalAccuracy*100)/100) Level:\(level)"
     }
     
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
